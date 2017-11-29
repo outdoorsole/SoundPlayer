f20e37f8c4b37c54f177e3e4a9ef02cbe69cd0ce
Maribel Montejano <outdoorsole@users.noreply.github.com>
2017-11-28 18:30:39 -0800

    Add guitarButton; add IBAction

--- SoundPlayer/ViewController.swift
+++ SoundPlayer/ViewController.swift
@@ -18,11 +18,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func guitarButton(_ sender: UIButton) {
+        print("Guitar button tapped")
     }
-
 
 }
 
