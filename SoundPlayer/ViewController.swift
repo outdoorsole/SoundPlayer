3a78270c197993c65b489c6d94b4305b2d800d22
Maribel Montejano <outdoorsole@users.noreply.github.com>
2017-11-28 18:28:16 -0800

    Create player property for VC

--- SoundPlayer/ViewController.swift
+++ SoundPlayer/ViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var player: AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
